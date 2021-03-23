aeef1eee2d6b53dca798d151cd8f928dd43a5866
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-03-23 09:47:52 -0700

    add formula

---
+++ Formula/memocalc.rb
@@ -0,0 +1,11 @@
+class MemoCalc < Formula
+    desc "A useful tool for practicing mental mathematics."
+    homepage "https://github.com/memomath/memocalc"
+    url "https://github.com/memomath/memocalc/releases/download/latest/memocalc-1.0.0-x86_64-apple-darwin.tar.gz"
+    sha256 "66c91d621b3d229f583bc27826b0b3dc1e54335f"
+    version "1.0.0"
+  
+    def install
+      bin.install "memocalc"
+    end
+  end
