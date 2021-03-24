f0659299c856d02d20c631a7a1c4425732a9c1d4
Gautam <68178572+eternalmoon1234@users.noreply.github.com>
2021-03-23 17:51:49 -0700

    update download url

--- Formula/memocalc.rb
+++ Formula/memocalc.rb
@@ -1,7 +1,7 @@
 class Memocalc < Formula
     desc "A useful tool for practicing mental mathematics."
     homepage "https://github.com/memomath/memocalc"
-    url "https://github.com/memomath/memocalc/releases/download/latest/memocalc-1.0.0-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/memomath/memocalc/releases/latest/download/memocalc-1.0.0-x86_64-apple-darwin.tar.gz"
     sha256 "66c91d621b3d229f583bc27826b0b3dc1e54335f"
     version "1.0.0"
   
